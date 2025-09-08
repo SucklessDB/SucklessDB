665340cf6c37a3be52c0e765ae8a85f0d4963c7d
Vilém Raška <v.rashka.ext@make.com>
2025-09-08 10:58:15 +0200

    wip: connection

--- src-tauri/src/commands/connection.rs
+++ src-tauri/src/commands/connection.rs
@@ -1,15 +1,18 @@
 use keyring::Entry;
 use specta::specta;
-use tauri::command;
+use tauri::{command, AppHandle};
+use tauri_plugin_store::StoreExt;
 
-use crate::constants::SERVICE_NAME;
+use crate::constants::{SERVICE_NAME, CONNECTIONS_FILE_NAME};
 
 #[specta]
 #[command]
-pub fn connect_to_db(id: String) -> String {
-    let _entry = Entry::new(&SERVICE_NAME, &id).unwrap();
+pub fn connect_to_db(app: AppHandle, id: String) -> String {
+    let store = app.store(&CONNECTIONS_FILE_NAME).unwrap();
+    let entry = Entry::new(&SERVICE_NAME, &id).unwrap();
 
-
+    let _connection = store.get(&id).expect("Can not get connection from store");
+    let _password = entry.get_password().expect("Can not get password from keyring");
 
     id
 }
